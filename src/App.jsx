72592cbf255682cabb8e7a6bef0df624052aaaf5
A-hil <hilevro@gmail.com>
2026-02-06 20:00:31 +0300

    Vercel: Изменение ссылок обноввление конфигов

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from './components/Header';
 import Slider from './components/Slider';
 import Footer from './components/Footer';
 import WhyUs from './components/WhyUs';
-import Certificates from './components/Certificates';
+import Certificates from './components/Certificates.jsx'
 
 function App() {
   return (
--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   build: {
     outDir: 'dist',
-    sourcemap: false,
     rollupOptions: {
-      output: {
-        manualChunks: undefined
+      input: {
+        main: './index.html'
       }
     }
-  },
-  server: {
-    port: 3000
   }
 })
